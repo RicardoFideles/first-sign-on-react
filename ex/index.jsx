844ea1762435ea1f52f720e3d4d00c74c9754525
Ricardo Fideles <ricardo@ricardofideles.com.br>
2017-08-01 14:33:20 -0300

    working with parent and child components

--- ex/index.jsx
+++ ex/index.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Primeiro, {Segundo} from './component'
+import Family from './family';
+import Member from './member';
 
-ReactDOM.render(<h1>Olá,  React</h1>, document.getElementById('app'));
+ReactDOM.render(
+    <div>
+        {/* <Primeiro value="show!"/>
+        <Segundo value="bah!"/> */}
+        <Family lastName='Down'>
+            <Member name='Jhon' />
+            <Member name='Jane' />
+            <Member name='Ja' />
+        </Family>
+    </div>, 
+document.getElementById('app'));
 
